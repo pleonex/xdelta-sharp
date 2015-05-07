2cf9cbdf6c845109e5a052ef63da935e2451f032
Benito Palacios Sánchez <benito356@gmail.com>
2015-05-07 14:02:07 +0200

    Implement a custom binary reader for VCDIFF integers

---
+++ xdelta.UnitTests/VcdReaderTests.cs
@@ -0,0 +1,352 @@
+﻿//
+//  VcdiffReaderTests.cs
+//
+//  Author:
+//       Benito Palacios Sánchez <benito356@gmail.com>
+//
+//  Copyright (c) 2015 Benito Palacios Sánchez
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Xdelta.UnitTests
+{
+    [TestFixture]
+    public class VcdReaderTests
+    {
+        VcdReader reader;
+        Stream stream;
+
+        [SetUp]
+        public void SetUp()
+        {
+            stream = new MemoryStream();
+            reader = new VcdReader(stream);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            stream.Dispose();
+        }
+
+        private void WriteBytes(params byte[] data)
+        {
+            stream.Write(data, 0, data.Length);
+            stream.Position = 0;
+        }
+
+        [Test]
+        public void ReadByteWithExactSize()
+        {
+            WriteBytes(0x10);
+            byte actual = reader.ReadUInt8();
+            Assert.AreEqual(0x10, actual);
+        }
+
+        [Test]
+        public void ReadByteWithMoreBytes()
+        {
+            WriteBytes(0x81, 0x1E);
+            byte actual = reader.ReadUInt8();
+            Assert.AreEqual(0x9E, actual);
+            Assert.AreEqual(2, stream.Position);
+        }
+
+        [Test]
+        public void ReadByteWithOverflowBits()
+        {
+            WriteBytes(0x80, 0x81);
+            Assert.Throws<FormatException>(
+                () => reader.ReadUInt8(),
+                "overflow in decode_integer");
+        }
+
+        [Test]
+        public void ReadByteWithOverflowValue()
+        {
+            WriteBytes(0x83, 0x01);
+            Assert.Throws<FormatException>(
+                () => reader.ReadUInt8(),
+                "overflow in decode_integer");
+        }
+
+        [Test]
+        public void ReadSByteWithExactSize()
+        {
+            WriteBytes(0x7F);
+            sbyte actual = reader.ReadInt8();
+            Assert.AreEqual(0x7F, actual);
+        }
+
+        [Test]
+        public void ReadSByteWithMoreBytes()
+        {
+            WriteBytes(0x81, 0x00);
+            sbyte actual = reader.ReadInt8();
+            Assert.AreEqual(-128, actual);
+            Assert.AreEqual(2, stream.Position);
+        }
+
+        [Test]
+        public void ReadSByteWithOverflowBits()
+        {
+            WriteBytes(0x80, 0x81);
+            Assert.Throws<FormatException>(
+                () => reader.ReadInt8(),
+                "overflow in decode_integer");
+        }
+
+        [Test]
+        public void ReadSByteWithOverflowValue()
+        {
+            WriteBytes(0x83, 0x1);
+            Assert.Throws<FormatException>(
+                () => reader.ReadInt8(),
+                "overflow in decode_integer");
+        }
+
+        [Test]
+        public void ReadUInt16WithExactSize()
+        {
+            WriteBytes(0x84, 0x00);
+            ushort actual = reader.ReadUInt16();
+            Assert.AreEqual(0x200, actual);
+        }
+
+        [Test]
+        public void ReadUInt16WithMoreBytes()
+        {
+            WriteBytes(0x82, 0x81, 0x40);
+            ushort actual = reader.ReadUInt16();
+            Assert.AreEqual(0x80C0, actual);
+            Assert.AreEqual(3, stream.Position);
+        }
+
+        [Test]
+        public void ReadUInt16WithOverflowBits()
+        {
+            WriteBytes(0x80, 0x81, 0x80);
+            Assert.Throws<FormatException>(
+                () => reader.ReadUInt16(),
+                "overflow in decode_integer");
+        }
+
+        [Test]
+        public void ReadUInt16WithOverflowValue()
+        {
+            WriteBytes(0x84, 0x81, 0x00);
+            Assert.Throws<FormatException>(
+                () => reader.ReadUInt16(),
+                "overflow in decode_integer");
+        }
+
+        [Test]
+        public void ReadInt16WithExactSize()
+        {
+            WriteBytes(0x84, 0x00);
+            short actual = reader.ReadInt16();
+            Assert.AreEqual(0x200, actual);
+        }
+
+        [Test]
+        public void ReadInt16WithMoreBytes()
+        {
+            WriteBytes(0x82, 0x80, 0x00);
+            short actual = reader.ReadInt16();
+            Assert.AreEqual(-32768, actual);
+            Assert.AreEqual(3, stream.Position);
+        }
+
+        [Test]
+        public void ReadInt16WithOverflowBits()
+        {
+            WriteBytes(0x80, 0x81, 0x80);
+            Assert.Throws<FormatException>(
+                () => reader.ReadInt16(),
+                "overflow in decode_integer");
+        }
+
+        [Test]
+        public void ReadInt16WithOverflowValue()
+        {
+            WriteBytes(0x84, 0x81, 0x00);
+            Assert.Throws<FormatException>(
+                () => reader.ReadInt16(),
+                "overflow in decode_integer");
+        }
+
+        [Test]
+        public void ReadUInt32WithExactSize()
+        {
+            WriteBytes(0xBA, 0xEF, 0x9A, 0x15);
+            uint actual = reader.ReadUInt32();
+            Assert.AreEqual(123456789, actual);
+        }
+
+        [Test]
+        public void ReadUInt32WithMoreBytes()
+        {
+            WriteBytes(0x88, 0x80, 0x80, 0x80, 0x00);
+            uint actual = reader.ReadUInt32();
+            Assert.AreEqual(0x80000000, actual);
+            Assert.AreEqual(5, stream.Position);
+        }
+
+        [Test]
+        public void ReadUInt32WithOverflowBits()
+        {
+            WriteBytes(0x80, 0x80, 0x80, 0x80, 0x80);
+            Assert.Throws<FormatException>(
+                () => reader.ReadUInt32(),
+                "overflow in decode_integer");
+        }
+
+        [Test]
+        public void ReadUInt32WithOverflowValue()
+        {
+            WriteBytes(0x90, 0x80, 0x80, 0x80, 0x80);
+            Assert.Throws<FormatException>(
+                () => reader.ReadUInt32(),
+                "overflow in decode_integer");
+        }
+
+        [Test]
+        public void ReadInt32WithExactSize()
+        {
+            WriteBytes(0xBA, 0xEF, 0x9A, 0x15);
+            int actual = reader.ReadInt32();
+            Assert.AreEqual(123456789, actual);
+        }
+
+        [Test]
+        public void ReadInt32WithMoreBytes()
+        {
+            WriteBytes(0x88, 0x80, 0x80, 0x80, 0x00);
+            int actual = reader.ReadInt32();
+            Assert.AreEqual(Int32.MinValue, actual);
+            Assert.AreEqual(5, stream.Position);
+        }
+
+        [Test]
+        public void ReadInt32WithOverflowBits()
+        {
+            WriteBytes(0x80, 0x80, 0x80, 0x80, 0x80);
+            Assert.Throws<FormatException>(
+                () => reader.ReadInt32(),
+                "overflow in decode_integer");
+        }
+
+        [Test]
+        public void ReadInt32WithOverflowValue()
+        {
+            WriteBytes(0x90, 0x80, 0x80, 0x80, 0x80);
+            Assert.Throws<FormatException>(
+                () => reader.ReadInt32(),
+                "overflow in decode_integer");
+        }
+
+
+        [Test]
+        public void ReadUInt64WithExactSize()
+        {
+            WriteBytes(0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01);
+            ulong actual = reader.ReadUInt64();
+            Assert.AreEqual(0x01, actual);
+        }
+
+        [Test]
+        public void ReadUInt64WithMoreBytes1()
+        {
+            WriteBytes(0xC0, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01);
+            ulong actual = reader.ReadUInt64();
+            Assert.AreEqual(0x4000000000000001, actual);
+            Assert.AreEqual(9, stream.Position);
+        }
+
+        [Test]
+        public void ReadUInt64WithMoreBytes2()
+        {
+            WriteBytes(0x81, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01);
+            ulong actual = reader.ReadUInt64();
+            Assert.AreEqual(0x8000000000000001, actual);
+            Assert.AreEqual(10, stream.Position);
+        }
+
+        [Test]
+        public void ReadUInt64WithOverflowBits()
+        {
+            WriteBytes(0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00);
+            Assert.Throws<FormatException>(
+                () => reader.ReadUInt64(),
+                "overflow in decode_integer");
+            Assert.AreEqual(10, stream.Position);
+        }
+
+        [Test]
+        public void ReadUInt64WithOverflowValue()
+        {
+            WriteBytes(0x82, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00);
+            ulong actual = reader.ReadUInt64();
+            Assert.AreEqual(0x00, actual);
+        }
+
+        [Test]
+        public void ReadInt64WithExactSize()
+        {
+            WriteBytes(0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01);
+            long actual = reader.ReadInt64();
+            Assert.AreEqual(0x01, actual);
+        }
+
+        [Test]
+        public void ReadInt64WithMoreBytes1()
+        {
+            WriteBytes(0xC0, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01);
+            long actual = reader.ReadInt64();
+            Assert.AreEqual(0x4000000000000001, actual);
+            Assert.AreEqual(9, stream.Position);
+        }
+
+        [Test]
+        public void ReadInt64WithMoreBytes2()
+        {
+            WriteBytes(0x81, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00);
+            long actual = reader.ReadInt64();
+            Assert.AreEqual(Int64.MinValue, actual);
+            Assert.AreEqual(10, stream.Position);
+        }
+
+        [Test]
+        public void ReadInt64WithOverflowBits()
+        {
+            WriteBytes(0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00);
+            Assert.Throws<FormatException>(
+                () => reader.ReadInt64(),
+                "overflow in decode_integer");
+            Assert.AreEqual(10, stream.Position);
+        }
+
+        [Test]
+        public void ReadInt64WithOverflowValue()
+        {
+            WriteBytes(0x82, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00);
+            long actual = reader.ReadInt64();
+            Assert.AreEqual(0x00, actual);
+        }
+    }
+}
+
---
+++ xdelta/VcdReader.cs
@@ -0,0 +1,111 @@
+﻿//
+//  VcdiffReader.cs
+//
+//  Author:
+//       Benito Palacios Sánchez <benito356@gmail.com>
+//
+//  Copyright (c) 2015 Benito Palacios Sánchez
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.IO;
+
+namespace Xdelta
+{
+    public class VcdReader : BinaryReader
+    {
+        public VcdReader(Stream stream)
+            : base(stream)
+        {
+        }
+
+        public sbyte ReadInt8()
+        {
+            return (sbyte)DecodeInteger(1); 
+        }
+
+        public byte ReadUInt8()
+        {
+            return (byte)DecodeInteger(1);
+        }
+
+        public override short ReadInt16()
+        {
+            return (short)DecodeInteger(2);
+        }
+
+        public override ushort ReadUInt16()
+        {
+            return (ushort)DecodeInteger(2);
+        }
+
+        public override int ReadInt32()
+        {
+            return (int)DecodeInteger(4);
+        }
+
+        public override long ReadInt64()
+        {
+            return (long)DecodeInteger(8);
+        }
+
+        public override uint ReadUInt32()
+        {
+            return (uint)DecodeInteger(4);
+        }
+
+        public override ulong ReadUInt64()
+        {
+            return DecodeInteger(8);
+        }
+
+        /// <summary>
+        /// Decodes an integer of variable length.
+        /// Defined in RCF-3284, the VCDIFF format uses a variable-length integer.
+        /// Each byte contains 7-bits of data and the last bit indicates if there
+        /// more bytes to decode.
+        /// </summary>
+        /// <returns>The integer.</returns>
+        /// <param name="maxBytes">Max bytes of the output format.</param>
+        private ulong DecodeInteger(int maxBytes)
+        {
+            // There is no way to constraint the generic to a number so I am
+            // using the biggest value possible and a casting at the end.
+            // http://stackoverflow.com/questions/32664/is-there-a-constraint-that-restricts-my-generic-method-to-numeric-types
+            ulong value = 0;
+            ulong mask = ~((1ul << (maxBytes * 8)) - 1u);
+            int bitsRead = 0;
+
+            byte data;
+            do {
+                // If it had already the opportinuty to read the maximum value
+                // This prevent read past the end of the stream
+                if (bitsRead > maxBytes * 8)
+                    throw new FormatException("overflow in decode_integer");
+
+                data = ReadByte();
+
+                bitsRead += 7;  // Bits of data read
+                value = (value << 7) | (data & 0x7Fu);  // Data only in the first 7 bits
+
+                // Check the maximum value expected (expect for long)
+                if (maxBytes != 8 && (value & mask) != 0)
+                    throw new FormatException("overflow in decode_integer");
+            } while (data >> 7 == 1);   // Continue bit in the last bit
+
+            return value;
+        }
+    }
+}
+
